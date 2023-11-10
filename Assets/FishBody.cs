4a42ce6400f6509431feb34c9941198b53d7e9b9
MaxFrymark <94791525+MaxFrymark@users.noreply.github.com>
2023-11-10 15:02:23 -0500

    Bug Fix
    -Fixed Enemy detection bug

--- Assets/FishBody.cs
+++ Assets/FishBody.cs
@@ -6,7 +6,7 @@ public class FishBody : MonoBehaviour
 {
     [SerializeField] EnemyFish fish;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
